62f766f6dcbf5e5cf5a47a3554be296b18627829
Laurent Bernabe <laurent.bernabe@gmail.com>
2020-06-13 19:50:50 +0200

    Refactoring #2

--- chessboard/renderer.go
+++ chessboard/renderer.go
@@ -31,53 +31,7 @@ func (renderer Renderer) Layout(size fyne.Size) {
 	cellsSize := fyne.Size{Width: int(cellsLength), Height: int(cellsLength)}
 
 	renderer.drawCellsAndPieces(size)
-
-	coordsFontSize := int(float64(cellsLength) * 0.25)
-
-	fileCoordsOffset := int(float64(cellsLength) * 0.95)
-	rankCoordsOffset := int(float64(cellsLength) * 0.8)
-
-	for file := 0; file < 8; file++ {
-		var x int
-		if renderer.boardWidget.blackSide == BlackAtTop {
-			x = fileCoordsOffset + cellsLength*file
-		} else {
-			x = fileCoordsOffset + cellsLength*(7-file)
-		}
-		yTop := int(float64(cellsLength) * 0.015)
-		yBottom := int(float64(cellsLength) * 8.515)
-
-		topCoord := renderer.filesCoords[0][file]
-		topCoord.TextStyle = fyne.TextStyle{Bold: true}
-		topCoord.TextSize = coordsFontSize
-		topCoord.Move(fyne.Position{X: x, Y: yTop})
-
-		bottomCoord := renderer.filesCoords[1][file]
-		bottomCoord.TextStyle = fyne.TextStyle{Bold: true}
-		bottomCoord.TextSize = coordsFontSize
-		bottomCoord.Move(fyne.Position{X: x, Y: yBottom})
-	}
-
-	for rank := 0; rank < 8; rank++ {
-		var y int
-		if renderer.boardWidget.blackSide == BlackAtTop {
-			y = rankCoordsOffset + cellsLength*rank
-		} else {
-			y = rankCoordsOffset + cellsLength*(7-rank)
-		}
-		xLeft := int(float64(cellsLength) * 0.2)
-		xRight := int(float64(cellsLength) * 8.7)
-
-		leftCoord := renderer.ranksCoords[0][rank]
-		leftCoord.TextStyle = fyne.TextStyle{Bold: true}
-		leftCoord.TextSize = coordsFontSize
-		leftCoord.Move(fyne.Position{X: xLeft, Y: y})
-
-		rightCoord := renderer.ranksCoords[1][rank]
-		rightCoord.TextStyle = fyne.TextStyle{Bold: true}
-		rightCoord.TextSize = coordsFontSize
-		rightCoord.Move(fyne.Position{X: xRight, Y: y})
-	}
+	renderer.drawCoordinates(size)
 
 	turnCirclePlace := int(float64(cellsLength) * 8.5)
 	turnCircle := renderer.playerTurn
@@ -182,5 +136,57 @@ func (renderer Renderer) drawCellsAndPieces(size fyne.Size) {
 				currentPiece.Move(cellPosition)
 			}
 		}
+	}
+}
+
+func (renderer Renderer) drawCoordinates(size fyne.Size) {
+	minSize := math.Min(float64(size.Width), float64(size.Height))
+	cellsLength := int(minSize / 9.0)
+
+	coordsFontSize := int(float64(cellsLength) * 0.25)
+
+	fileCoordsOffset := int(float64(cellsLength) * 0.95)
+	rankCoordsOffset := int(float64(cellsLength) * 0.8)
+
+	for file := 0; file < 8; file++ {
+		var x int
+		if renderer.boardWidget.blackSide == BlackAtTop {
+			x = fileCoordsOffset + cellsLength*file
+		} else {
+			x = fileCoordsOffset + cellsLength*(7-file)
+		}
+		yTop := int(float64(cellsLength) * 0.015)
+		yBottom := int(float64(cellsLength) * 8.515)
+
+		topCoord := renderer.filesCoords[0][file]
+		topCoord.TextStyle = fyne.TextStyle{Bold: true}
+		topCoord.TextSize = coordsFontSize
+		topCoord.Move(fyne.Position{X: x, Y: yTop})
+
+		bottomCoord := renderer.filesCoords[1][file]
+		bottomCoord.TextStyle = fyne.TextStyle{Bold: true}
+		bottomCoord.TextSize = coordsFontSize
+		bottomCoord.Move(fyne.Position{X: x, Y: yBottom})
+	}
+
+	for rank := 0; rank < 8; rank++ {
+		var y int
+		if renderer.boardWidget.blackSide == BlackAtTop {
+			y = rankCoordsOffset + cellsLength*rank
+		} else {
+			y = rankCoordsOffset + cellsLength*(7-rank)
+		}
+		xLeft := int(float64(cellsLength) * 0.2)
+		xRight := int(float64(cellsLength) * 8.7)
+
+		leftCoord := renderer.ranksCoords[0][rank]
+		leftCoord.TextStyle = fyne.TextStyle{Bold: true}
+		leftCoord.TextSize = coordsFontSize
+		leftCoord.Move(fyne.Position{X: xLeft, Y: y})
+
+		rightCoord := renderer.ranksCoords[1][rank]
+		rightCoord.TextStyle = fyne.TextStyle{Bold: true}
+		rightCoord.TextSize = coordsFontSize
+		rightCoord.Move(fyne.Position{X: xRight, Y: y})
 	}
 }
